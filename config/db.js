200c734521959f1bf0c6082ec5b8b2cb11fec63a
mu-osama <muhammad.osama@pixeledge.io>
2026-02-22 11:35:30 +0500

    fix: db connection

--- config/db.js
+++ config/db.js
@@ -4,6 +4,7 @@ const MONGO_URI = process.env.MONGO_URI;
 
 const connectDB = async () => {
   try {
+    if (mongoose.connection.readyState >= 1) return;
     await mongoose.connect(MONGO_URI);
     console.log("DB Connected");
   } catch (error) {
